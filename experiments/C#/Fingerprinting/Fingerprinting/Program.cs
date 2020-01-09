028d215359232dce8a10107ab2827e2a6044d316
Owliie <sladurcheto5232@abv.bg>
2020-01-10 01:50:50 +0200

    Fixed a bug in the fingerprinting algorithm Found the best epsilon and sampling rate combination

--- experiments/C#/Fingerprinting/Fingerprinting/Program.cs
+++ experiments/C#/Fingerprinting/Fingerprinting/Program.cs
@@ -10,7 +10,7 @@ namespace Fingerprinting
     class Program
     {
         private static List<int> frequencies;
-        
+
         private static byte[] GetBytesOfSong(string songPath)
         {
             var songFile = new AudioFileReader(songPath);
@@ -105,9 +105,21 @@ namespace Fingerprinting
         private static List<long> Fingerprint(Complex32[][] sampled)
         {
             var size = sampled.GetLength(0);
-            var peaks = Enumerable.Repeat(Enumerable.Repeat(0L, frequencies.Count - 1).ToList(), size).ToList();
-            var highest = Enumerable.Repeat(Enumerable.Repeat(0F, frequencies.Count).ToList(), size).ToList();
-            var hashed = Enumerable.Repeat(0L, size).ToList();
+            var peaks = new List<List<long>>();
+            for (int i = 0; i < size; i++)
+            {
+                peaks.Add(new List<long>(new long[frequencies.Count - 1]));
+            }
+            var highest = new List<List<double>>();
+            for (int i = 0; i < size; i++)
+            {
+                highest.Add(new List<double>(new double[frequencies.Count - 1]));
+            }
+            var hashed = new List<long>(new long[size]);
+
+//            var peaks = Enumerable.Repeat(Enumerable.Repeat(0L, frequencies.Count - 1).ToList(), size).ToList();
+//            var highest = Enumerable.Repeat(Enumerable.Repeat(0F, frequencies.Count).ToList(), size).ToList();
+//            var hashed = Enumerable.Repeat(0L, size).ToList();
 
             for (int i = 0; i < sampled.GetLength(0); i++)
             {
@@ -116,9 +128,9 @@ namespace Fingerprinting
                     var magnitude = sampled[i][frequency].Magnitude;
                     var index = GetRangeOfFrequency(frequency);
 
-                    if (magnitude > highest[i][index])
+                    if (magnitude > highest[i][index - 1])
                     {
-                        highest[i][index] = magnitude;
+                        highest[i][index - 1] = magnitude;
                         peaks[i][index - 1] = frequency;
                     }
                 }
@@ -166,27 +178,45 @@ namespace Fingerprinting
             var epicBeginningPath = @"C:\git\Smart-Radio\experiments\songs\epic_part_beginning.mp3";
             var epicPartPath = @"C:\git\Smart-Radio\experiments\songs\epic_part.mp3";
             var pinkoPath = @"C:\git\Smart-Radio\experiments\songs\pinko.mp3";
+            var eminemPath = @"C:\git\Smart-Radio\experiments\songs\eminem.mp3";
+            var eminem1Path = @"C:\git\Smart-Radio\experiments\songs\eminem1.mp3";
+            var eminem2Path = @"C:\git\Smart-Radio\experiments\songs\eminem2.mp3";
+            var eminem3Path = @"C:\git\Smart-Radio\experiments\songs\eminem3.mp3";
 
-            var epsilons = new long[] { 2174106058, 12000000, 13000000, 14000000, 15000000, 16000000, 17000000, 18000000, 19000000, 20000000, 21000000 };
+            var epsilons = new long[] { 2174106058, 3174106058,
+//                12174106058, 2174106058, 12000000, 13000000, 14000000,
+//                15000000, 16000000, 17000000, 18000000, 19000000, 20000000, 21000000,
+                26000000, 23000000, 24000000, 29000000, 30000000,
+//                23000000, 24000000, 25000000, 26000000,
+                27000000 };
 
-            var sampleSize = 1024;
+            // DON'T CHANGE (512: 24000000-27000000 -> best performance)
+            var sampleSize = 512;
 
             var epic = Fingerprint(Sample(GetBytesOfSong(epicPath), sampleSize));
             var epicPart = Fingerprint(Sample(GetBytesOfSong(epicPartPath), sampleSize));
-//            var epicBeginning = Fingerprint(Sample(GetBytesOfSong(epicBeginningPath), sampleSize));
-//            var pinko = Fingerprint(Sample(GetBytesOfSong(pinkoPath), sampleSize));
-            Console.WriteLine(epic);
-            Console.WriteLine(epicPart);
-
-//            foreach (var epsilon in epsilons)
-//            {
-//                Console.WriteLine();
-//                Console.WriteLine($"epsilon: {epsilon}");
-//                Console.WriteLine("epic - epic part: " + MatchMaking(epic, epicPart, epsilon) * 100 / epicPart.Count + "%");
-//                Console.WriteLine("epic - epic beginning: " + MatchMaking(epic, epicBeginning, epsilon) * 100 / epicBeginning.Count + "%");
-//                Console.WriteLine("pinko - epic part: " + MatchMaking(pinko, epicPart, epsilon) * 100 / epicPart.Count + "%");
-//                Console.WriteLine("pinko - epic beginning: " + MatchMaking(pinko, epicBeginning, epsilon) * 100 / epicBeginning.Count + "%");
-//            }
+            var epicBeginning = Fingerprint(Sample(GetBytesOfSong(epicBeginningPath), sampleSize));
+            var pinko = Fingerprint(Sample(GetBytesOfSong(pinkoPath), sampleSize));
+            var eminem = Fingerprint(Sample(GetBytesOfSong(eminemPath), sampleSize));
+            var eminem1 = Fingerprint(Sample(GetBytesOfSong(eminem1Path), sampleSize));
+            var eminem2 = Fingerprint(Sample(GetBytesOfSong(eminem2Path), sampleSize));
+            var eminem3 = Fingerprint(Sample(GetBytesOfSong(eminem3Path), sampleSize));
+//            Console.WriteLine(String.Join(" ", epic.Take(20)));
+//            Console.WriteLine(String.Join(" ", epicBeginning.Take(20)));
+//            Console.WriteLine("epic - epic beginning: " + MatchMaking(epic, epicBeginning, 22174106058) * 100 / epicBeginning.Count + "%");
+            
+            foreach (var epsilon in epsilons)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"epsilon: {epsilon}");
+                Console.WriteLine("epic - epic part: " + MatchMaking(epic, epicPart, epsilon) * 100 / epicPart.Count + "%");
+                Console.WriteLine("epic - epic beginning: " + MatchMaking(epic, epicBeginning, epsilon) * 100 / epicBeginning.Count + "%");
+                Console.WriteLine("pinko - epic part: " + MatchMaking(pinko, epicPart, epsilon) * 100 / epicPart.Count + "%");
+                Console.WriteLine("pinko - epic beginning: " + MatchMaking(pinko, epicBeginning, epsilon) * 100 / epicBeginning.Count + "%");
+                Console.WriteLine("eminem - eminem 1: " + MatchMaking(eminem, eminem1, epsilon) * 100 / eminem1.Count + "%");
+                Console.WriteLine("eminem - eminem 2: " + MatchMaking(eminem, eminem2, epsilon) * 100 / eminem2.Count + "%");
+                Console.WriteLine("eminem - eminem 3: " + MatchMaking(eminem, eminem3, epsilon) * 100 / eminem3.Count + "%");
+            }
         }
     }
 }
