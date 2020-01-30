feaf67248b91cf69ed52cc8d588db1372be17bb1
Owliie <sladurcheto5232@abv.bg>
2020-01-30 22:16:03 +0200

    Added clear search option

---
+++ SmartRadio/SmartRadio/wwwroot/js/searchIndex.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function() {
+    $("#appendix").prepend($("<input class='btn btn-warning' type='button' onclick='redirectToMusic()' value='X'/>"));
+});
+
+function redirectToMusic() {
+    location.href = '/Music';
+}
