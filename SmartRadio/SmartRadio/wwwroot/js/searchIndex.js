4fec3c942daf45cc09a5c9d2b645669ffdcbd7fb
Owliie <sladurcheto5232@abv.bg>
2020-03-21 19:54:41 +0200

    Added pagination when displaying search users results

--- SmartRadio/SmartRadio/wwwroot/js/searchIndex.js
+++ SmartRadio/SmartRadio/wwwroot/js/searchIndex.js
@@ -1,7 +1,61 @@
-﻿$(document).ready(function() {
+﻿var page = 0;
+var maxPages = 0;
+var peoplePerPage = 8;
+var peopleCount = 0;
+
+$(document).ready(function () {
     $("#appendix").prepend($("<input class='btn btn-important' type='button' onclick='redirectToMusic()' value='X'/>"));
+    $("#prev-arrow").prop("disabled", true);
+
+    peopleCount = $("#results").children().length;
+
+    maxPages = Math.ceil(peopleCount / peoplePerPage);
+
+    if (peopleCount <= peoplePerPage) {
+        $("#search-navigation").hide();
+
+        for (var person of $("#results").children()) {
+            $(person).attr("hidden", false);
+        }
+    } else {
+        for (var i = 0; i < peoplePerPage; i++) {
+            $($("#results").children()[i]).attr("hidden", false);
+        }
+    }
 });
 
 function redirectToMusic() {
     location.href = '/Music';
+}
+
+function navigatePrevious() {
+    $("#next-arrow").prop("disabled", false);
+
+    shouldShowPeople(true);
+
+    page--;
+    if (page === 0) {
+        $("#prev-arrow").prop("disabled", true);
+    }
+
+    shouldShowPeople(false);
+}
+
+function navigateNext() {
+    $("#prev-arrow").prop("disabled", false);
+
+    shouldShowPeople(true);
+
+    page++;
+    if (page === maxPages - 1) {
+        $("#next-arrow").prop("disabled", true);
+    }
+
+    shouldShowPeople(false);
+}
+
+function shouldShowPeople(show) {
+    for (let i = peoplePerPage * page; i < peoplePerPage * page + peoplePerPage; i++) {
+        $($("#results").children()[i]).attr("hidden", show);
+    }
 }
